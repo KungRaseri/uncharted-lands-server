750fd3b932017b979dfeb96e35a926dbb71f9879
KungRaseri <countjoshington@gmail.com>
2023-02-17 11:06:11 -0800

    initial commit of server from main repo

---
+++ api/Hubs/GameHub.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace api.Hubs;
+
+public class GameHub : Hub
+{
+    private readonly GameManager _gameManager;
+
+    public GameHub(GameManager gameManager)
+    {
+        _gameManager = gameManager;
+    }
+}
