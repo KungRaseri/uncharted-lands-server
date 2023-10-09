b2707244c5a9cf0dfc38774c5a0122c992b51045
KungRaseri <countjoshington@gmail.com>
2023-10-08 23:17:27 -0700

    replaced webapi with BackgroundService, added EFCore, NPgSQL, WorkerService, etc.

---
+++ UnchartedLands/WorkerService/Data/Account.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkerService.Data
+{
+    public class Account
+    {
+        [Key]
+        [Column("id")]
+        public string Id { get; set; }
+        public string email;
+        public string passwordHash;
+        public string userAuthToken;
+        public string role;
+
+        public DateTime createdAt;
+        public DateTime updatedAt;
+    }
+}
---
+++ UnchartedLands/WorkerService/Data/WorkerDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorkerService.Data
+{
+    public class WorkerDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DbSet<Account> Accounts { get; private set; }
+
+        public WorkerDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().ToTable(nameof(Account));
+
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("Database"));
+        }
+    }
+}
